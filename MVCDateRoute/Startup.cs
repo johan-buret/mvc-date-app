5f15a18f0ee6bf58f0e7861780e06866d5fcf984
Johan Buret <johan.buret@free.fr>
2016-03-26 01:18:21 +0100

    Premier import

---
+++ MVCDateRoute/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCDateRoute.Startup))]
+namespace MVCDateRoute
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
