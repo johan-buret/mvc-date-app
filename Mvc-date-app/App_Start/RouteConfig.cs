5cda05ac4416b758f71ca62c66f235275328a072
Johan Buret <johan.buret@free.fr>
2016-04-02 16:21:50 +0200

    Date parsing

---
+++ Mvc-date-app/App_Start/RouteConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Globalization;
+
+namespace Mvc_date_app
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Date"
+                , url: "{controller}/{action}/bydate/{date}"
+                , defaults: new { controller = "Home", action = "Index", datespecialformat = UrlParameter.Optional }
+                , constraints: new { datespecialformat = @"\d{12}" }).RouteHandler = new DateRouteHandler();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+
+
+    public class DateRouteHandler : MvcRouteHandler
+    {
+        protected override IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            var dateValue = requestContext.RouteData.Values["date"];
+            if(dateValue!=null)
+            {
+                var dateS = dateValue.ToString();
+                requestContext.RouteData.Values["date"] = DateTime.ParseExact(dateS, "yyyyMMddHHmm", CultureInfo.InvariantCulture.DateTimeFormat);
+            }
+            return base.GetHttpHandler(requestContext);
+        }
+    }
+}
